893602f0f80d79e134cd2739bba8de9c969394b3
Dikshit Bhardwaj <54199681+iamdikshit@users.noreply.github.com>
2020-10-26 08:10:21 -0700

    Add files via upload

---
+++ includes/adminheader.php
@@ -0,0 +1,34 @@
+
+    <!-- header -->
+    <div class="header-top">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-12 col-lg-6 d-flex">
+            <a href="index.php" class="site-logo">
+              <img src="includes/web_logo.png"  class="img-fluid" style=" width: 50%; height: auto;">
+            </a>
+
+
+          </div>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            
+                     <a href="index.php" class="nav-link text-left" style="color: white;background-color: black;border-radius: 30px;">Home</a>&nbsp;
+                   
+                
+                    <a href="logout.php" class="nav-link text-left" style="color: white;background-color: black;border-radius: 30px;">Logout</a>&nbsp;
+               
+                
+                    <a href="reports.php" class="nav-link text-left" style="color: white;background-color: black;border-radius: 30px;">Daily Reports</a>&nbsp;
+                
+                <a href="details.php" class="nav-link text-left" style="color: white;background-color: black;border-radius: 30px;">Details</a></li>&nbsp;
+               
+            
+          
+        </div>
+      </div>
+    </div>
+      
+        
+        <!--header end-->
+        
+        
---
+++ includes/footer.php
@@ -0,0 +1,40 @@
+<div class=" subscribe bg-light">
+    <div class="container">
+        <form action="#" class="row align-items-center">
+          <div class="col-md-5 mr-auto">
+              <center>
+                <img src="images/ourlogo.png"  class="img-fluid" style=" width: auto; height: auto;">
+                <p>We are covering the facts of the story from different perspectives </p>
+              </center>
+          </div>
+            <div class="col-md-5 ml-auto">
+            
+                <center>
+                <p style="color: black;font: bold">Follow us on social Media</p>
+                <a href="https://www.facebook.com/TheStoryADDA/" class="d-inline-block p-3"><span class="icon-facebook"></span></a>
+                <a href="https://twitter.com/TheStoryADDA" class="d-inline-block p-3"><span class="icon-twitter"></span></a>
+                <a href="https://www.instagram.com/thestoryadda/" class="d-inline-block p-3"><span class="icon-instagram"></span></a>
+                <a href="#" class="d-inline-block p-3"><span class="icon-youtube"></span></a>
+                    
+            </center>
+            
+      </div>
+            <form>
+                </div>
+</div>
+
+
+
+<div class="footer">
+      <div class="container">
+        
+
+        <div class="row">
+          <div class="col-12">
+            <div class="copyright">
+                <p> <b>Copyright © The Story ADDA.  All Rights Reserved | Contact Us thestoryadda@gmail.com</b> </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
